9467600bf1e9ecd151285b31ff79e9112d9aa14b
James <james_hadoop@163.com>
2016-04-13 23:35:21 +0800

    add curring

---
+++ src/james/scala/function/Curring.scala
@@ -0,0 +1,17 @@
+package james.scala.function
+
+object Curring {
+    def main(args: Array[String]) {
+        def multiple(x: Int, y: Int) = x * y
+        def multipleOne(x: Int) = (y: Int) => x * y
+        println(multipleOne(6))
+        println(multipleOne(6)(7))
+        println
+
+        def curring(x: Int)(y: Int) = x * y
+
+        val a = Array("Hello", "Spark")
+        val b = Array("hello", "spark")
+        println(a.corresponds(b)(_.equalsIgnoreCase(_)))
+    }
+}
